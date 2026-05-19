422472be6e578c57df315f9b8236525751739a48
caracciolo <andrea.caracciolo@exmachina.ch>
2026-05-19 16:18:50 +0200

    feature: markdown link replacement

--- backend/src/main/java/ch/exmachina/cosmo42/services/chat/processors/ConversationProcessor.java
+++ backend/src/main/java/ch/exmachina/cosmo42/services/chat/processors/ConversationProcessor.java
@@ -2,9 +2,12 @@ package ch.exmachina.cosmo42.services.chat.processors;
 
 import ch.exmachina.cosmo42.dto.ChatEventType;
 import ch.exmachina.cosmo42.dto.ChatResponseDTO;
+import ch.exmachina.cosmo42.entities.KBDocument;
+import ch.exmachina.cosmo42.repositories.KBDocumentRepository;
 import ch.exmachina.cosmo42.services.chat.ChatAttribute;
 import ch.exmachina.cosmo42.services.chat.ChatContext;
 import ch.exmachina.cosmo42.services.chat.tools.KBDocumentSimilaritySearchTool;
+import ch.exmachina.cosmo42.services.kb.MarkdownLinkProcessor;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,12 +15,14 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -29,6 +34,7 @@ public class ConversationProcessor implements ChatProcessor {
     OpenAiChatOptions.Builder chatModelOptionsBuilder;
     ChatMemory chatMemory;
     KBDocumentSimilaritySearchTool kbDocumentSimilaritySearchTool;
+    KBDocumentRepository kbDocumentRepository;
 
     private static final String system_instruction = """
                 You are cosmo42, an expert in retrieving information from a private knowledge base.
@@ -62,6 +68,9 @@ public class ConversationProcessor implements ChatProcessor {
                 .order(20)
                 .build();
 
+        MarkdownLinkProcessor markdownLinkProcessor = new MarkdownLinkProcessor();
+        List<KBDocument> allKbDocuments = kbDocumentRepository.findAll();
+
         return chatClient.prompt()
                 .user(context.getRequest().message())
                 .advisors(chatMemoryAdvisor)
@@ -69,11 +78,21 @@ public class ConversationProcessor implements ChatProcessor {
                 .toolContext(buildToolContext(context))
                 .stream()
                 .chatResponse()
-                .map(response -> {
-                    String text = (response.getResult() != null
-                            && response.getResult().getOutput().getText() != null)
-                            ? response.getResult().getOutput().getText()
-                            : "";
+                .bufferUntil(response -> response.getResult() != null &&
+                        response.getResult().getOutput().getText() != null &&
+                        response.getResult().getOutput().getText().endsWith("\n")) // buffer response tokens until newline character is encountered
+                .map(responses -> {
+                    String text = "";
+                    for (ChatResponse response : responses) {
+                        String raw = (response.getResult() != null
+                                && response.getResult().getOutput().getText() != null)
+                                ? response.getResult().getOutput().getText()
+                                : "";
+                        text += raw;
+                    }
+
+                    text = markdownLinkProcessor.replaceFileReferenceLinks(text, allKbDocuments);
+
                     return ServerSentEvent.<ChatResponseDTO>builder()
                             .data(ChatResponseDTO.builder()
                                     .type(ChatEventType.CHUNK)
---
+++ backend/src/main/java/ch/exmachina/cosmo42/services/kb/MarkdownLinkProcessor.java
@@ -0,0 +1,39 @@
+package ch.exmachina.cosmo42.services.kb;
+
+import ch.exmachina.cosmo42.entities.KBDocument;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+public class MarkdownLinkProcessor{
+
+    public String replaceFileReferenceLinks(String messageChunk, List<KBDocument> allKBDocument) {
+        String newMessageChunk = messageChunk;
+        for(KBDocument kbDocument : allKBDocument) {
+            newMessageChunk = replaceReferenceLink(newMessageChunk, kbDocument);
+        }
+        return newMessageChunk;
+    }
+
+    private String replaceReferenceLink(String chunkContent, KBDocument kbDocument) {
+        String result = chunkContent;
+        String targetLink = buildMarkdownLink(kbDocument.getFileName(), kbDocument.getUuid());
+        String refVariant1 = "(?i)REF_FILE_"+kbDocument.getUuid();
+        String refVariant2 = "(?i)REF_FILE_"+kbDocument.getFileName();
+        String refVariant3 = "(?i)REF_FILE_"+kbDocument.getFileName().replaceAll(" ", "_");
+        String refVariant4 = "(?i)REF_FILE_"+kbDocument.getFileName().replaceAll(" ", "_").replaceAll(".", "_");
+        String refVariant5 = MessageFormat.format("(?<=[^\\[])file-name 1.doc(?=[^\\]])", kbDocument.getFileName());
+        result = result.replaceAll(refVariant1, targetLink);
+        result = result.replaceAll(refVariant2, targetLink);
+        result = result.replaceAll(refVariant3, targetLink);
+        result = result.replaceAll(refVariant4, targetLink);
+        result = result.replaceAll(refVariant5, targetLink);
+        return result;
+    }
+
+    private String buildMarkdownLink(String refName, String uuid){
+        return MessageFormat.format("[{0}](/api/v1/kb/documents/{1}/download)", refName, uuid);
+    }
+
+
+}
