3ec22a4b78a0cb7d71394bb8b116b5de34950633
caracciolo <andrea.caracciolo@exmachina.ch>
2026-05-20 10:25:26 +0200

    address PR comments

--- backend/src/main/java/ch/exmachina/cosmo42/services/kb/MarkdownLinkProcessor.java
+++ backend/src/main/java/ch/exmachina/cosmo42/services/kb/MarkdownLinkProcessor.java
@@ -23,7 +23,7 @@ public class MarkdownLinkProcessor{
         String refVariant2 = "REF_FILE_"+kbDocument.getFileName();
         String refVariant3 = "REF_FILE_"+kbDocument.getFileName().replace(" ", "_");
         String refVariant4 = "REF_FILE_"+kbDocument.getFileName().replace(" ", "_").replace(".", "_");
-        String refVariant5 = MessageFormat.format("(?<=[^\\[]){0}(?=[^\\]])", Pattern.quote(kbDocument.getFileName()));
+        String refVariant5 = MessageFormat.format("(?<=[^\\[*]){0}(?=[^\\]])", Pattern.quote(kbDocument.getFileName()));
         result = result.replace(refVariant1, targetLink);
         result = result.replace(refVariant2, targetLink);
         result = result.replace(refVariant3, targetLink);
@@ -31,6 +31,7 @@ public class MarkdownLinkProcessor{
         result = result.replaceAll(refVariant5, targetLink);
         return result;
     }
+
 
     private String buildMarkdownLink(String refName, String uuid){
         return MessageFormat.format("[{0}](/api/v1/kb/documents/{1}/download)", refName, uuid);
