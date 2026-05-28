5cdfd307f888694fb311a62532685cadb99ff45c
Marco Celeri <marco.celeri@exmachina.ch>
2026-05-28 11:16:14 +0200

    Improved properties logic and fixed CD/CI

--- backend/src/main/java/ch/exmachina/cosmo42/controllers/ConfigController.java
+++ backend/src/main/java/ch/exmachina/cosmo42/controllers/ConfigController.java
@@ -1,32 +1,27 @@
 package ch.exmachina.cosmo42.controllers;
 
+import ch.exmachina.cosmo42.config.FeatureFlagsProperties;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/config")
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 @Tag(name = "Config", description = "Configuration endpoints")
 public class ConfigController {
 
-    @Value("${cosmo42.features.studio:false}")
-    boolean studioFeatureEnabled;
+    private final FeatureFlagsProperties props;
 
     @GetMapping("/features")
     @Operation(summary = "Get feature flags")
     public ResponseEntity<Map<String, Boolean>> getFeatureFlags() {
-        Map<String, Boolean> features = new HashMap<>();
-        features.put("studio", studioFeatureEnabled);
-        return ResponseEntity.ok(features);
+        return ResponseEntity.ok(props.getFlags());
     }
 }
